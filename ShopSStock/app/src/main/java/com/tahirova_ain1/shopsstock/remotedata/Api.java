073eba7aa31acb4558a422006730197fd8a69ac2
Perchik <saikalrustamovna@gmail.com>
2024-05-26 20:05:49 +0600

    fully works

--- ShopSStock/app/src/main/java/com/tahirova_ain1/shopsstock/remotedata/Api.java
+++ ShopSStock/app/src/main/java/com/tahirova_ain1/shopsstock/remotedata/Api.java
@@ -9,7 +9,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Api {
-    @GET("api/v1/user/all")
+    @GET("api/v1/auth/users")
     Call<List<User>> getAllUsers();
     @GET("api/v1/order/all")
     Call<List<Order>> getAllOrders();
